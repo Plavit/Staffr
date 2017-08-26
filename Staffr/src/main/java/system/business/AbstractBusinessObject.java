3c6fb14208219b5c8883ed1cbb201afaf8c8f56e
Marek Szeles <szelemar@fel.cvut.cz>
2017-08-26 22:07:45 +0200

    BO init

---
+++ Staffr/src/main/java/system/business/AbstractBusinessObject.java
@@ -0,0 +1,26 @@
+package system.business;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+/**
+ * Created by MarekS on 26.08.2017.
+ */
+
+@MappedSuperclass
+public abstract class AbstractBusinessObject implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    protected int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
