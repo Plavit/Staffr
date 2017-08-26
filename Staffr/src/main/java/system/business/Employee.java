9f44723a5cbee89fc75fb05efadf7ce6fff7596b
Krystof Sykora <sykorkry@fel.cvut.cz>
2017-08-26 23:00:29 +0200

    project is now... MAVENIZED!

---
+++ Staffr/src/main/java/system/business/Employee.java
@@ -0,0 +1,43 @@
+package system.bo;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Employee {
+
+    @Column()
+    String firstName;
+
+    @Column()
+    String lastName;
+
+    @Column()
+    int birthYear;
+
+    @Column
+    private String email;
+
+    @Column
+    private String note;
+
+    @Column
+    private int phoneNumber;
+
+    //TODO: isSupervisorOf
+
+    @OneToMany()
+    @JoinTable(name = "EMPLOYEE_DEGREE",
+            joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "DEGREE_ID", referencedColumnName = "ID"))
+    private Set<Object> employeeDegrees;
+
+
+    @OneToOne
+    @JoinTable(name = "EMPLOYEE_POSITION",
+            joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"),
+            inverseJoinColumns = @JoinColumn(name = "POSITION_ID", referencedColumnName = "ID"))
+    private Set<Object> employeePosition;
+
+    // TODO: Missing relations to other classes
+}
