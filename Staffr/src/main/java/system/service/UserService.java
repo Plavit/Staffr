d9c3f6051a94c7d7ed3e336f89613da9e2ed4859
Krystof Sykora <sykorkry@fel.cvut.cz>
2017-10-01 23:14:57 +0200

    login autorisation

--- Staffr/src/main/java/system/service/UserService.java
+++ Staffr/src/main/java/system/service/UserService.java
@@ -1,9 +1,12 @@
 package system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import system.business.User;
 import system.dao.UserDao;
+
+import java.util.List;
 
 /**
  * Created by krystof on 9/24/17.
@@ -12,13 +15,42 @@ import system.dao.UserDao;
 @Service
 public class UserService {
     @Autowired
-    private UserDao userDao;
+    UserDao userDao;
 
-    public void persist(User t) {
-        userDao.persist(t);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean exists(String username) {
+        return userDao.findByUsername(username) != null;
     }
 
-    public void update(User t){
-        userDao.update(t);
+    public void persist(User user) {
+        if (exists(user.getUserName())) {
+            //TODO: USERNAME ALLREADY IN USE
+        }else {
+            try {
+                user.encodePassword(passwordEncoder);
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+            userDao.persist(user);
+        }
+    }
+
+    public void update(User user){
+        if (exists(user.getUserName())) {
+            //TODO: USERNAME DOES NOT EXIST
+        }else {
+            try {
+                user.encodePassword(passwordEncoder);
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            }
+            userDao.update(user);
+        }
+    }
+
+    public void remove(int id){
+        userDao.deleteById(id);
     }
 }
