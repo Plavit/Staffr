b20917b0e7ba76d2a4d17d5c12349989f498486c
Marek Szeles <szelemar@fel.cvut.cz>
2018-01-09 18:07:43 +0100

    added css init

--- Staffr/src/main/webapp/js/component/LoginPage.js
+++ Staffr/src/main/webapp/js/component/LoginPage.js
@@ -4,6 +4,10 @@ import {Alert, Button, Panel} from "react-bootstrap";
 import {Link, Router, hashHistory} from "react-router";
 import axios from "axios";
 import querystring from "querystring";
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Paper, TextField} from "material-ui";
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -58,16 +62,24 @@ export default class Login extends React.Component {
 
     render() {
         return (
+            <MuiThemeProvider>
             <div>
+
                 <h1 style={{color: '#FF0000'}}>Hello, my name is elder Price. And I have come to show you the most amazing book.</h1>
+                <Paper zDepth={3} >
                 <form className='form-horizontal'>
-                    <p><label for="username">Username: </label><input type='text' name='username' id='username'
-                                                                      onChange={this.onChange}/></p>
-                    <p><label for="password">Password: </label><input type='password' name='password' id='password'
-                                                                      onChange={this.onChange}/></p>
-                    <p><input type='button' name="submit" value="login" onClick={this.login.bind(this)}/></p>
-                </form>
+                    <p><label for="username">Username: </label>
+                        <TextField type='text' name='username' id='username' onChange={this.onChange}/>
+
+                    </p>
+                    <p><label for="password">Password: </label>
+                        <TextField type='password' name='password' id='password' onChange={this.onChange}/>
+                    </p>
+                    <RaisedButton label="Login" primary={true}	name="submit" value="login" onClick={this.login.bind(this)}/>
+                    </form>
+                </Paper>
             </div>
+    </MuiThemeProvider>
         );
     }
 }
